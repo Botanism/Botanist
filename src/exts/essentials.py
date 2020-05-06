6c7ae915ae6674e4a18ac8664ea1cc38bf9df05d
s0lst1ce <thithib.cohergne@gmail.com>
2020-05-06 15:04:07 +0200

    fix #110

--- src/exts/essentials.py
+++ src/exts/essentials.py
@@ -108,8 +108,7 @@ class Essentials(commands.Cog):
 
     @commands.Cog.listener()
     async def on_guild_join(self, guild):
-        with open(os.path.join(CONFIG_FOLDER, f"{guild.id}.json"), "w") as file:
-            json.dump(DEFAULT_SERVER_FILE)
+        ConfigFile(guild.id, default=DEFAULT_SERVER_FILE)
         await guild.owner.send(
             f"I was just added to your server. For me to work correctly (or at all) on your server you should send `::init` in any channel of your {guild.name} server."
         )
@@ -167,7 +166,7 @@ class Essentials(commands.Cog):
     @commands.command()
     @has_auth("manager")
     async def clear(self, ctx, *filters):
-        """deletes specified <nbr> number of messages in the current channel"""
+        """deletes specified <nbr> number of messages in the current channel."""
         # building arguments
         filters = list(filters)
         nbr = None
@@ -198,7 +197,11 @@ class Essentials(commands.Cog):
         if period:
             hist_args["after"] = period
         if nbr and not members:
-            hist_args["limit"] = nbr + 1
+            nbr += 1
+            hist_args["limit"] = nbr
+        if members:
+            await ctx.message.delete()
+
         if not (period or nbr):
             raise discord.ext.commands.ArgumentParsingError(
                 "Can't delete all messages of a user!"
@@ -208,18 +211,18 @@ class Essentials(commands.Cog):
         now = datetime.datetime.now()
         async for msg in ctx.channel.history(**hist_args):
             if not members or msg.author in members:
-                local_logger.info(
+                local_logger.debug(
                     f"Deleting message {msg.jump_url} from guild {msg.guild.name}."
                 )
-                if (msg.created_at - now).days >= -14:
+                if (msg.created_at - now).days <= -14:
                     await msg.delete()
                 else:
                     to_del.append(msg)
 
                 if nbr != None:
+                    nbr -= 1
                     if nbr <= 0:
                         break
-                    nbr -= 1
 
         try:
             await ctx.channel.delete_messages(to_del)
